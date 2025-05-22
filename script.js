eb89a22b52565f0a4418736d8bd8d4b2f9fa8cc7
leisingha <120764124+leisingha@users.noreply.github.com>
2025-05-22 17:37:57 -0400

    fix hidden error issue

--- script.js
+++ script.js
@@ -2,3 +2,21 @@
 // span section to display error message
 // Don't submit the form to server.
 // form attribute 'novalidate'
+
+const form = document.querySelector('form');
+const email = document.querySelector('#email');
+const emailError = document.querySelector('.emailContainer span');
+
+email.addEventListener('input', () => {
+    if (email.validity.valid) {
+        emailError.className = '';
+        emailError.textContent = '';
+    } else {
+        emailError.textContent = 'The email field is wrong';
+        emailError.className = 'active';
+    }
+});
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+});
