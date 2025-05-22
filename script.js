138525b4700d5c4e9c9ac4afa38040482c80c55a
leisingha <120764124+leisingha@users.noreply.github.com>
2025-05-22 18:34:42 -0400

    install npm module

--- script.js
+++ script.js
@@ -4,19 +4,42 @@
 // form attribute 'novalidate'
 
 const form = document.querySelector('form');
+
 const email = document.querySelector('#email');
 const emailError = document.querySelector('.emailContainer span');
 
-email.addEventListener('input', () => {
-    if (email.validity.valid) {
-        emailError.className = '';
-        emailError.textContent = '';
-    } else {
-        emailError.textContent = 'The email field is wrong';
-        emailError.className = 'active';
-    }
-});
+const country = document.querySelector('#country');
+const countryError = document.querySelector('.countryContainer span');
+
+const postal = document.querySelector('#postal');
+const postalError = document.querySelector('.postalContainer span');
+
+const password = document.querySelector('#password');
+const passwordError = document.querySelector('.passwordContainer span');
+
+const rePassword = document.querySelector('#rePassword');
+const rePasswordError = document.querySelector('.rePasswordContainer span');
+
+email.addEventListener('input', handleError());
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 });
+
+function handleError() {}
+
+function checkEmail() {
+    if (!email.validity.valid) {
+        emailError.textContent = 'Incorrect email address';
+        emailError.classList = 'active';
+    } else {
+        emailError.textContent = '';
+        emailError.classList = '';
+    }
+}
+
+function checkCountry(){
+    if (!country.validity.valid || ) {
+
+    }
+}
